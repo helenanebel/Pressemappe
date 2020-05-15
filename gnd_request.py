b2ab4ad52c56673f9207f6a7b4e0642992f9bbd6
gadischke <>
2020-05-15 18:02:18 +0200

    Suche nach Name und Typ, speichern als Liste

--- gnd_request.py
+++ gnd_request.py
@@ -1,41 +1,48 @@
-import urllib
-from urllib import request
 import json
-import time
+import urllib
+from urllib import request, parse
 
-entities = ['London']
+with open("./entities.json", mode="r", encoding="utf-8") as file:
+    image_list = json.load(file)[0]
 
+for image in image_list:
+    for entity in image_list[image]:
+        search_url = "https://lobid.org/gnd/search?q="
 
-found_gnd_items = 0
-search_not_successfull = []
-missing_gnd_ids = {}
-ids_processed = 0
-for entity in entities:
-    success = False
-    #hier mal überprüfen, was da überhaupt in den Daten steht und was dann da rauskommt.
-    search_url = "https://lobid.org/gnd/search?q=" + entity
-    trials = 0
-    found=False
-    while success!=True:
-        if trials >= 10:
-            search_not_successfull.append(entity)
+        # erstmal nur Felder für Namen durchsuchen
+        search_url += urllib.parse.quote_plus("preferredName:")
+        search_url += "\"" + urllib.parse.quote_plus(entity["name"]) + "\""  # exakte Suche
+
+        search_url += "+OR+"
+
+        search_url += urllib.parse.quote_plus("variantName:")
+        search_url += urllib.parse.quote_plus(entity["name"])
+
+        # Typ
+        search_url += "&filter=" + urllib.parse.quote_plus("type:")
+        if entity["type"] == "PER":
+            search_url += "Person"
+        else:
+            search_url += "PlaceOrGeographicName"
+
+        # Menge und Ausgabeformat
+        search_url += "&size=1000&format=json"
+
+        # hier weitermachen
+
+        req = urllib.request.Request(search_url)
+        with urllib.request.urlopen(req) as response:
+            json_response = response.read()
+        json_response = json_response.decode('utf-8')
+        json_response = json.loads(json_response)
+
+        if json_response["totalItems"] < 1:
+            entity["possibleGndId"] = []
             break
-        try:
-            req = urllib.request.Request(search_url)
-            with urllib.request.urlopen(req) as response:
-                json_response=response.read()
-            json_response=json_response.decode('utf-8')
-            json_response=json.loads(json_response)
-            if json_response['totalItems']>0:
-                id = json_response['member'][0]['id']
-                found_gnd_items +=1
-                found=True
-                print(json_response)
-            time.sleep(1)
-            success=True
-        except Exception as e:
-            print('Error! Code: {c}, Message, {m}'.format(c = type(e).__name__, m = str(e)))
-            print('gescheitert:', search_url)
-            print(entity, entities[entity])
-            time.sleep(7)
-    ids_processed +=1
+        elif json_response["totalItems"] < 1001:
+            entity["possibleGndId"] = []
+            for gnd_entity in json_response["member"]:
+                entity["possibleGndId"].append(gnd_entity["gndIdentifier"])
+
+with open("./entities.json", mode="w+", encoding="utf-8") as file:
+    json.dump([image_list], file, indent=4)
