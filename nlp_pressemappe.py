fbdfaa2b4695103bd6a9e4f8506b378e5bc3661e
gundograebs <Santi!ago1>
2020-05-19 16:36:58 +0200

    jgghgj ddds

--- nlp_pressemappe.py
+++ nlp_pressemappe.py
@@ -1,17 +1,57 @@
-import tesseract_pressemappe
+import spacy
 import os
+import json
+from collections import Counter
+nlp = spacy.load('xx_ent_wiki_sm')
+docs ='path to files'
 
-tesseract_pressemappe.get_text_files()
+def get_filename(path):
+    return [i.path for i in os.scandir(path) if i.is_file()]
 
-file_list = os.listdir('pressemappe_text_files')
+files=get_filename(docs)
+final_dict = {}
+for filepath in files:
+
+    with open(filepath, 'r', encoding='UTF8') as file_to_read:
+        some_text = file_to_read.read()
+        base_name = os.path.basename(filepath)
+        print(base_name)
+        doc = nlp(some_text)
+        perlist=[]
+        loclist=[]
+        for ent in doc.ents:
+             if ent.label_ == "PER":
+                perlist.append(str(ent))
+             elif ent.label_ == "LOC":
+                loclist.append(str(ent))
+        final_list = []
+
+        # Entitäten ählen (PER)
+        c = Counter(perlist)
+        for p, count in c.most_common():
+            final_list.append({
+                'name': p,
+                'type': 'PER',
+                'frequency': count
+            })
+
+        # Entitäten zählen LOC)
+        c = Counter(loclist)
+        for l, count in c.most_common():
+            final_list.append({
+                'name': l,
+                'type': 'LOC',
+                'frequency': count
+            })
 
 
-def get_entities_from_text(file_list):
-    entities_list = []
-    ...
+        final_dict[base_name] = final_list
 
-    return entities_list
+        print('Final result', final_dict)
 
 
-if __name__ == '__main__':
-    get_entities_from_text(file_list)
+with open('entities.json', mode="r+") as file:
+    file.seek(0, 2)
+    position = file.tell()
+    file.seek(position)
+    file.write(",{}".format(json.dumps(final_dict, indent=1)))
