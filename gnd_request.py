bca621f0757c7d5ccb329877fa7130951ea8741e
gadischke <>
2020-06-23 13:30:01 +0200

    Fehler behoben und unscharfe Suche

--- gnd_request.py
+++ gnd_request.py
@@ -2,12 +2,12 @@ import json
 import os
 from urllib import request, parse
 from math import ceil
+#from nltk.corpus import stopwords
 
 
 with open("entities.json", mode="r", encoding="utf-8") as file:
     image_list = json.load(file)[0]
 
-# erstmal nur direkte Beziehungen ermitteln, daher nur Domain/Range spezifisch
 with open("relations.json", mode="r", encoding="utf-8") as file:
     relations = json.load(file)
 
@@ -27,42 +27,41 @@ def build_url(name, type):
         if counter != (len(name.split()) - 1):
             url += "+AND+"
 
-    # Individualisierte Person bzw. Geografikum
-    url += "&filter=" + parse.quote_plus("type:")
+    # Person (ohne Nicht-Individualisierte Person) bzw. Geografikum
     if type == "PER":
-        url += "DifferentiatedPerson"
+        url += "+AND+_exists_" + parse.quote_plus(":") + "preferredNameEntityForThePerson"
+        url += "&filter="
+        url += parse.quote_plus("type:") + "Person+AND+NOT+"
+        url += parse.quote_plus("type:") + "UndifferentiatedPerson"
+        url += parse.quote_plus("")
     else:
+        url += "&filter="
         url += "PlaceOrGeographicName"
     url += "&size=1000&format=json"
 
     return url
 
 
+# nur bis 10.000 Treffer erfassen
 def get_member(url):
     req = request.Request(url)
     with request.urlopen(req) as response:
         json_response = response.read()
     json_response = json_response.decode("utf-8")
     json_response = json.loads(json_response)
-
     count = json_response["totalItems"]
     start = 0
     member = []
-
-    for c in range(ceil(count / 1000)):
-
-        loop_url = url + "&from=" + str(start)
-        print(loop_url)
-        req = request.Request(loop_url)
-        with request.urlopen(req) as response:
-            json_response = response.read()
-        json_response = json_response.decode("utf-8")
-        json_response = json.loads(json_response)
-        member.extend(json_response["member"])
-        start += 1000
-        if start > 24000: # andere Methode nötig für mehr Daten
-            break
-
+    if count in range(1,10000):
+        for c in range(ceil(count / 1000)):
+            loop_url = url + "&from=" + str(start)
+            req = request.Request(loop_url)
+            with request.urlopen(req) as response:
+                json_response = response.read()
+            json_response = json_response.decode("utf-8")
+            json_response = json.loads(json_response)
+            member.extend(json_response["member"])
+            start += 1000
     return member
 
 
@@ -70,36 +69,27 @@ def get_data(member_list):
     member_list_slim = []
     for member in member_list:
         gnd = {"identifier": member["gndIdentifier"], "names": [], "relations": [], "descriptions": []}
-
         gnd["names"].append(member["preferredName"])
-        try:
+
+        if "variantName" in member:
             for name in member["variantName"]:
                 gnd["names"].append(name)
-        except KeyError:
-            continue
 
         for field in relations["relation"]:
-            try:
+            if field in member:
                 for relation in member[field]:
                     gnd["relations"].append(relation["id"][22:])
-            except KeyError:
-                continue
 
-        try:
+        if "geographicAreaCode" in member:
             for code in member["geographicAreaCode"]:
-                code = geo_codes[code["id"][59:]]
-                if code not in gnd["relations"] and code != gnd["identifier"]:
-                    gnd["relations"].append(code)
-        except KeyError:
-            continue
+                code = code["id"][59:]
+                if code in geo_codes and code not in gnd["relations"] and code != gnd["identifier"]:
+                    gnd["relations"].append(geo_codes[code])
 
         for field in relations["descriptions"]:
-            try:
+            if field in member:
                 for text in member[field]:
                     gnd["descriptions"].append(text)
-            except KeyError:
-                continue
-
         member_list_slim.append(gnd)
     return member_list_slim
 
@@ -110,9 +100,27 @@ for image in image_list:
 
         member_list = get_member(url)
 
+        # notfalls unscharfe Suche für jedes Zeichen
+        if not member_list:
+            for i, part in enumerate(entity["name"].split()):
+                namelist = entity["name"].split()
+                for index, char in enumerate(part):
+                    word = list(part)
+                    word[index] = "*"
+                    word = "".join(word)
+                    namelist[i] = word
+                    name = " ".join(namelist)
+                    url = build_url(name, entity["type"])
+
+                    for member in get_member(url):
+                        if member not in member_list:
+                            member_list.append(member)
         member_list = get_data(member_list)
 
         entity["possible_gnd"] = member_list
+
+        # hier später eindeutiges GND-Äquivalent speichern
+        entity["gnd"] = []
 
     with open("image_member/" + image.replace(".JPG", ".json"), "w+") as file:
         json.dump(image_list[image], file, indent=4)
