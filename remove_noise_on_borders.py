4cf002adba7181cf5fc28165429082337b5c5ff9
Helena_Nebel <Helena.Nebel@dainst.de>
2020-06-23 16:03:56 +0200

    Replace noise on borders by white pixels

--- remove_noise_on_borders.py
+++ remove_noise_on_borders.py
@@ -22,14 +22,15 @@ def rotate_by_degree(img, angle):
 
 
 picture_nr = 0
-for picture in ['pressemappe_results/test_5.JPG']:
+for picture in ['pressemappe_results/test_7.JPG']:
     print(picture)
     if picture_nr > 1000:
         break
     picture_nr += 1
-    img = cv.imread('pressemappe_results/test_5.JPG')
+    img = cv.imread('pressemappe_results/test_7.JPG')
     # print(img)
     gray = color_to_gray(img)
+    gray = rotate_by_degree(gray, -0.7)
     characters_in_line = False
     column = 0
     number_of_regions = int(gray.shape[0]/100)*2
@@ -43,14 +44,14 @@ for picture in ['pressemappe_results/test_5.JPG']:
             region_length = int(len(pixel_list)/number_of_regions)
             region_list = [pixel_list[i:i + region_length] for i in range(0, len(pixel_list), region_length)]
             regions_with_characters = [0 if statistics.mean(region)>=200 else 1 for region in region_list ]
-            if sum(regions_with_characters) <= len(region_list)*0.2:
+            if sum(regions_with_characters) <= len(region_list)*0.266:
                 region_nr = 0
                 for region in regions_with_characters:
                     if region_nr not in [0, 1, len(region_list)-1, len(region_list)-2]:
                         if sum(regions_with_characters[region_nr-2:region_nr+3]) > 2:
                             regions_with_characters[region_nr] = 1
                     region_nr += 1
-            if sum(regions_with_characters) <= len(region_list) * 0.2:
+            if sum(regions_with_characters) <= len(region_list) * 0.266:
                 columns_to_delete.append(column)
         starting = []
         for col in range(1, len(columns_to_delete) - 1):
@@ -75,12 +76,12 @@ for picture in ['pressemappe_results/test_5.JPG']:
     # alle Spalten in dieser Liste mit 255 auffüllen!
     # shape[0] ist width, shape[1] ist heigth
 
-    gray = rotate_by_degree(gray, -0.75)
+    #
     # herausfinden, was der perfekte Winkel ist (berechnen aus der Anzahl der schwarzen Pixel
     # wenn man die Regionen auswertet. (besser sind wenige Regionen
     # mit einem hohen Gesamtwert als viele Regionen mit einem niedrigen Gesamtwert!
     # noch versuchen, die Kante zu finden!!!
-
+    # closing vor dem Abschneiden ist sinnvoll!
     saveimg(gray, 'lala.JPG', 'jpgs_sharpened/') # geht nur mit .JPG am Ende.
 
     break
