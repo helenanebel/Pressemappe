a0ebc35e18ef6599d8c617e212a37e90bf1fa445
Helena_Nebel <Helena.Nebel@dainst.de>
2020-06-26 14:12:53 +0200

    Download jpgs from URLs

--- get_jpgs.py
+++ get_jpgs.py
@@ -17,7 +17,8 @@ def get_jpg_urls(jpg_amount: int = 2000):
     dir_page = dir_page.decode('utf-8')
     dir_soup = BeautifulSoup(dir_page, 'html.parser')
     for per_folder in [tr for tr in [tr.find('a')['href'] for tr in dir_soup.find_all('tr') if tr.find('a')] if 'xx' in tr]:
-        if jpg_nr >= 2000:
+        if jpg_nr >= jpg_amount:
+            jpg_urls = jpg_urls[:jpg_amount]
             break
         per_folder_url = 'https://zbw.eu/beta/pm20mets/pe/' + per_folder
         req = urllib.request.Request(per_folder_url)
@@ -26,7 +27,7 @@ def get_jpg_urls(jpg_amount: int = 2000):
         per_folder_page = per_folder_page.decode('utf-8')
         per_folder_soup = BeautifulSoup(per_folder_page, 'html.parser')
         for per_xml_url in [per_folder_url + tr for tr in [tr.find('a')['href'] for tr in per_folder_soup.find_all('tr') if tr.find('a')] if '.xml' in tr]:
-            if jpg_nr >= 2000:
+            if jpg_nr >= jpg_amount:
                 break
             req = urllib.request.Request(per_xml_url)
             with urllib.request.urlopen(req) as response:
@@ -34,15 +35,17 @@ def get_jpg_urls(jpg_amount: int = 2000):
             per_xml_page = per_xml_page.decode('utf-8')
             per_xml_soup = BeautifulSoup(per_xml_page, 'xml')
             jpg_urls += [img_tag['xlink:href'].replace('B.JPG', 'A.JPG') for img_tag in per_xml_soup.find('mets:fileGrp', USE="DEFAULT").find_all('mets:FLocat', LOCTYPE="URL")]
-            jpg_nr += 1
+            jpg_nr = len(jpg_urls)
+            print(jpg_nr)
     return jpg_urls
 
 
-def get_jpgs(jpg_url_list: list = get_jpg_urls(2000),
+def get_jpgs(jpg_url_list: list = get_jpg_urls(250),
              path_name: str = 'C://Users/Helena_Nebel/PycharmProjects/Pressemappe'):
     try:
         if 'jpgs' not in os.listdir(path_name):
             os.mkdir('jpgs')
+        print(len(jpg_url_list))
         for jpg_url in jpg_url_list:
             jpg_name = jpg_url.replace('http://webopac.hwwa.de/DigiPerson/P/', '')
             jpg_name = jpg_name.replace('/', '_')
