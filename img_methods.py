efa38b57af451981c860fb739c64765a317d3884
Helena_Nebel <Helena.Nebel@dainst.de>
2020-07-13 20:36:42 +0200

    Methods for treating images

--- img_methods.py
+++ img_methods.py
@@ -11,3 +11,9 @@ def save_img(img, img_name, output_path):
 
 def color_to_gray(img):
     return cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+
+def rotate_by_degree(img, angle):
+    return cv.warpAffine(img, cv.getRotationMatrix2D((img.shape[1]/2, img.shape[0]/2), angle, 1.0), (img.shape[1], img.shape[0]), borderValue=255)
+
+
