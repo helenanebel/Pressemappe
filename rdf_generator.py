bc795f24ff807993961cf859d95a04ef2d24aede
gadischke <>
2020-07-15 14:25:13 +0200

    Namespace geändert

--- rdf_generator.py
+++ rdf_generator.py
@@ -1,7 +1,7 @@
 import os
 import json
 from rdflib import Graph, Namespace, Literal, URIRef
-from rdflib.namespace import RDF, DC
+from rdflib.namespace import RDF, DCTERMS
 
 
 total = []
@@ -17,7 +17,7 @@ graph = Graph()
 
 GND = Namespace("https://d-nb.info/standards/elementset/gnd#")
 graph.bind("gndo", GND)
-graph.bind("dc", DC)
+graph.bind("dcterms", DCTERMS)
 
 for image_name in os.listdir("image_entities"):
     with open("image_entities/" + image_name, "r") as file:
@@ -26,9 +26,8 @@ for image_name in os.listdir("image_entities"):
     image_name = image_name.replace(".json", "")
     url = "http://webopac.hwwa.de/DigiPerson/P/" + image_name.replace("_", "/", 5) + ".JPG"
 
-    graph.add((URIRef(url), RDF.type, DC.BibliographicResource))
+    graph.add((URIRef(url), RDF.type, DCTERMS.BibliographicResource))
 
-    # hier Beziehung zu Ressource und Ressource selbst ergänzen
 
     for entity in image:
         if "gnd" in entity:
@@ -41,13 +40,13 @@ for image_name in os.listdir("image_entities"):
 
             graph.add((URIRef("https://d-nb.info/gnd/" + entity_id), GND.preferredName, Literal(entity["gnd"]["names"][0])))
 
-            graph.add((URIRef("https://d-nb.info/gnd/" + entity_id), DC.isReferencedBy, URIRef(url)))
-            graph.add((URIRef(url), DC.references, URIRef("https://d-nb.info/gnd/" + entity_id)))
+            graph.add((URIRef("https://d-nb.info/gnd/" + entity_id), DCTERMS.isReferencedBy, URIRef(url)))
+            graph.add((URIRef(url), DCTERMS.references, URIRef("https://d-nb.info/gnd/" + entity_id)))
 
             for relation_id in entity["gnd"]["relations"]:
                 if relation_id in total and relation_id != entity_id:
-                    graph.add((URIRef("https://d-nb.info/gnd/" + entity_id), DC.relation, URIRef("https://d-nb.info/gnd/" + relation_id)))
-                    graph.add((URIRef("https://d-nb.info/gnd/" + relation_id), DC.relation, URIRef("https://d-nb.info/gnd/" + entity_id)))
+                    graph.add((URIRef("https://d-nb.info/gnd/" + entity_id), DCTERMS.relation, URIRef("https://d-nb.info/gnd/" + relation_id)))
+                    graph.add((URIRef("https://d-nb.info/gnd/" + relation_id), DCTERMS.relation, URIRef("https://d-nb.info/gnd/" + entity_id)))
 
 graph.serialize(format="xml", destination="entities.rdf", encoding="utf-8")
 graph.serialize(format="turtle", destination="entities.ttl", encoding="utf-8")
