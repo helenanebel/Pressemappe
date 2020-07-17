326d8892a20430cad2b6e85311ef19791b197214
Helena_Nebel <Helena.Nebel@dainst.de>
2020-07-17 12:47:35 +0200

    Introduce function merge_images

---
+++ OCR/merge_images.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+from OCR.img_methods import save_img
+from OCR.tesseract_pressemappe import get_text_files_from_jpgs
+from OCR.get_contours import get_contours
+
+
+def merge_pictures(picture: str, do_save_img: bool = True):
+    img_1 = cv.imread('jpgs_sharpened/' + picture)
+    gray_1 = cv.cvtColor(img_1, cv.COLOR_BGR2GRAY)
+    print(gray_1.shape)
+    img_2 = get_contours(picture, source_dir_path='jpgs_cut')
+    print(img_2.shape)
+    dst = cv.bitwise_and(gray_1, img_2)
+    if do_save_img:
+        save_img(dst, picture, 'jpgs_added/')
+    return dst
+
+
+if __name__ == '__main__':
+    merge_pictures('0000xx_000010_000xx_00001_PIC_P000010000000000000000010000_0000_00000000HP_A.JPG')
